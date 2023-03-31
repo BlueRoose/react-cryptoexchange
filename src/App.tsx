cc1240c888178cae23932e9c67913b28527d3e14
Egor Zhuk <royalecool460@gmail.com>
2023-03-31 17:41:00 +0300

    Fixed css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Case from "./components/Case/Case";
+import ModalWindow from "./components/ModalWindow/ModalWindow";
 import CurrencyPage from "./pages/CurrencyPage/CurrencyPage";
 import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <Case />
+      <MainPage />
     </div>
   );
 }
